bf3d955c937e9629eb0721a86290bce4658f42e0
Yurii Kit <insidelkit@gmail.com>
2025-07-03 20:03:00 +0300

    first - init project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+
+// import { initMongoDB } from './db/initMongoDB.js';
+import { setupServer } from './server.js';
+
+const bootstrap = async () => {
+  //   await initMongoDB();
+  setupServer();
+};
+
+bootstrap();

47e6e219a4f9248fa063ad1e3a62f55835228c69
Yurii Kit <insidelkit@gmail.com>
2025-07-06 12:03:29 +0300

    end

---
+++ src/db/models/contact.js
@@ -0,0 +1,36 @@
+// src/db/models/student.js
+
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// export const ContactsCollection = model('contacts', contactsSchema);
+export const ContactsCollection = model('Contact', contactsSchema, 'contacts');
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // src/index.js
 
-// import { initMongoDB } from './db/initMongoDB.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
-  //   await initMongoDB();
+  await initMongoConnection();
   setupServer();
 };
 

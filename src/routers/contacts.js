f8737577622bd687060ded660a512dab31804819
Yurii Kit <insidelkit@gmail.com>
2025-07-16 12:34:37 +0300

    step2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,17 +8,41 @@ import {
   deleteContactController,
   updateContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+} from '../validation/contacts.js';
+
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactsController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactsController),
+);
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(updateContactController));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(updateContactSchema),
+  ctrlWrapper(updateContactController),
+);
 
 export default router;
---
+++ src/validation/contacts.js
@@ -0,0 +1,22 @@
+import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string()
+    .pattern(/^\+380\d{9}$/)
+    .required(),
+  email: Joi.string().email().required(),
+  contactType: Joi.string().valid('personal', 'home', 'work').required(),
+
+  isFavourite: Joi.boolean(),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string().pattern(/^\+380\d{9}$/),
+
+  email: Joi.string().email(),
+  contactType: Joi.string().valid('personal', 'home', 'work'),
+
+  isFavourite: Joi.boolean(),
+});
